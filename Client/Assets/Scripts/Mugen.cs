d9644c6e71ee8def581b656fb73acc27ca7ba32b
Tony Nguyen <tony@mugen>
2012-06-12 23:51:34 +1000

    Box that counts touches and follows first finger

--- Client/Assets/Scripts/Mugen.cs
+++ Client/Assets/Scripts/Mugen.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Mugen: MonoBehaviour
 {
-	private Rect _rect = new Rect(0, 0 , 100, 100);
+	private const int WIDTH = 70;
+	private const int HEIGHT = 70;
+	
+	private Rect _rect = new Rect(
+		(Screen.width-WIDTH)/2, (Screen.height-HEIGHT)/2, WIDTH, HEIGHT);
 	
 	private void OnGUI()
 	{
-		GUI.Box(_rect, "Mugen");
+		if (Input.touchCount > 0)
+		{
+			_rect.x = Input.touches[0].position.x;
+			_rect.y = (Screen.height - Input.touches[0].position.y);
+		}
+	
+		GUI.Box(_rect, "Mugen : " + Input.touchCount);
 	}
 }
